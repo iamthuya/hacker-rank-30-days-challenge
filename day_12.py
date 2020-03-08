b1ddbb4e5277ca1800aefbb2f20fd7e24a821605
Thu Ya Kyaw <thuyakyaw@yahoo.com>
2020-03-08 11:23:22 +0800

    added day 11

--- day_12.py
+++ day_12.py
@@ -1,29 +1,55 @@
-def hour_glass_sum(arr, i, j):
-    line_1 = arr[i-1][j-1] + arr[i-1][j] + arr[i-1][j+1]
-    line_2 = arr[i][j]
-    line_3 = arr[i+1][j-1] + arr[i+1][j] + arr[i+1][j+1]
+class Person:
+    def __init__(self, first_name, last_name, id_number):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.id_number = id_number
 
-    return line_1 + line_2 + line_3
+    def print_person(self):
+        print("Name:", self.last_name + ",", self.first_name)
+        print("ID:", self.id_number)
 
 
-def main():
-    arr = [
-        [1, 1, 1, 0, 0, 0],
-        [0, 1, 0, 0, 0, 0],
-        [1, 1, 1, 0, 0, 0],
-        [0, 0, 2, 4, 4, 0],
-        [0, 0, 0, 2, 0, 0],
-        [0, 0, 1, 2, 4, 0]
-    ]
+class Student(Person):
+    #   Class Constructor
+    #
+    #   Parameters:
+    #   firstName - A string denoting the Person's first name.
+    #   lastName - A string denoting the Person's last name.
+    #   id - An integer denoting the Person's ID number.
+    #   scores - An array of integers denoting the Person's test scores.
+    #
+    # Write your constructor here
+    def __init__(self, first_name, last_name, id_number, scores):
+        super().__init__(first_name, last_name, id_number)
+        self.scores = scores
 
-    largest = float('-Inf')
-    for i in range(1, len(arr) - 1):
-        for j in range(1, len(arr[i]) - 1):
-            value = hour_glass_sum(arr, i, j)
-            largest = value if value > largest else largest
-
-    print(largest)
+    #   Function Name: calculate
+    #   Return: A character denoting the grade.
+    #
+    # Write your function here
+    def calculate(self):
+        score = sum(self.scores) / len(self.scores)
+        if 90 <= score <= 100:
+            return "O"
+        elif 80 <= score < 90:
+            return "E"
+        elif 70 <= score < 80:
+            return "A"
+        elif 55 <= score < 70:
+            return "P"
+        elif 40 <= score < 55:
+            return "D"
+        else:
+            return "T"
 
 
 if __name__ == "__main__":
-    main()
+    line = input().split()
+    firstName = line[0]
+    lastName = line[1]
+    idNum = line[2]
+    numScores = int(input())  # not needed for Python
+    scores = list(map(int, input().split()))
+    s = Student(firstName, lastName, idNum, scores)
+    s.printPerson()
+    print("Grade:", s.calculate())
